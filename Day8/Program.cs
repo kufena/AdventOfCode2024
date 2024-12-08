670f873b523da0f1511a9c4bfd9a3cf9e0910058
Andrew Douglas <fairtomiddlingster@gmail.com>
2024-12-08 16:52:58 +0000

    Part 2 completed and ReadMe added.

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -28,7 +28,54 @@ for (int row = 0; row < rows; row++)
 
 Console.WriteLine($"{antennas.Keys.Count} antennas");
 
-Part1(rows, cols, antennas);
+// Part1(rows, cols, antennas);
+Part2(rows, cols, antennas);
+
+static void Part2(int rows, int cols, Dictionary<char, List<(int, int)>> antennas)
+{
+    var antinodes = new Dictionary<char, HashSet<(int, int)>>();
+
+    foreach (var ant in antennas.Keys)
+    {
+        antinodes.Add(ant, new());
+        var positions = antennas[ant];
+        foreach ((int c1, int r1) in positions)
+        {
+            foreach ((int c2, int r2) in positions)
+            {
+                if (c1 == c2 && r1 == r2)
+                    continue;
+                int coldiff = c1 - c2;
+                int rowdiff = r1 - r2;
+
+                int anticol = 0;
+                int antirow = 0;
+                anticol = c1 + coldiff; // (coldiff > 0) ? c2 + coldiff : (coldiff < 0 ? c1 + coldiff : -1);
+                antirow = r1 + rowdiff; // (rowdiff > 0) ? r2 + rowdiff : (rowdiff < 0 ? r1 + rowdiff : -1);
+                antinodes[ant].Add((c1, r1));
+                antinodes[ant].Add((c2, r2));
+                while (anticol >= 0 && anticol < cols && antirow >= 0 && antirow < rows) // && !uniqueAntennas.Contains((anticol,antirow)))
+                {
+                    antinodes[ant].Add((anticol, antirow));
+                    anticol += coldiff;
+                    antirow += rowdiff;
+                }
+            }
+        }
+    }
+
+    HashSet<(int, int)> uniquePositions = new();
+    foreach (var key in antinodes.Keys)
+    {
+        Console.WriteLine($"{key} has {antinodes[key].Count} antinodes.");
+        foreach (var pair in antinodes[key])
+        {
+            Console.WriteLine($"{key} antinode at {pair.Item1} {pair.Item2}");
+            uniquePositions.Add(pair);
+        }
+    }
+    Console.WriteLine($"Total of {uniquePositions.Count} unique antinode squares.");
+}
 
 static void Part1(int rows, int cols, Dictionary<char, List<(int, int)>> antennas)
 {
