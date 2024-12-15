3037b477bbee7a0bf56cd9bc49933f3575137475
Andrew Douglas <fairtomiddlingster@gmail.com>
2024-12-15 22:15:30 +0000

    Different approach - have got horizontal movement working ok, now to do the vertical.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,5 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static void ExpandGrid(string[] lines, int rows, int cols, ref int robotx, ref int roboty, HashSet<(int, int)> wallBlocks, HashSet<(int, int)> boxBlocks)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        var line = lines[i];
+        //        grid[i] = new char[cols*2];
+        for (int j = 0; j < cols; j++)
+        {
+            if (line[j] == '@') // it's the robot.
+            {
+                robotx = j * 2;
+                roboty = i;
+                //grid[i][j*2] = '@';
+                //grid[i][(j*2) + 1] = '.';
+            }
+            else if (line[j] == 'O')
+            {
+                //grid[i][j*2] = '[';
+                //grid[i][(j*2) + 1] = ']';
+                boxBlocks.Add((i, j * 2)); // just record the [ bit.
+            }
+            else if (line[j] == '#')
+            {
+                wallBlocks.Add((i, j * 2));
+                wallBlocks.Add((i, (j * 2) + 1));
+                //grid[i][j * 2] = line[j];
+                //grid[i][(j * 2) + 1] = line[j];
+            }
+            else
+            {
+                //grid[i][j * 2] = line[j];
+                //grid[i][(j * 2) + 1] = line[j];
+            }
+        }
+    }
+}
+
 var lines = File.ReadAllLines(args[0]);
 int k = 0;
 
@@ -24,6 +62,27 @@ Console.WriteLine($"{rows} rows by {cols} columns.");
 
 //Part1(lines, directions, rows, cols);
 Part2(lines, directions, rows, cols);
+
+void displayPosiGrid(HashSet<(int,int)> wallBlocks, HashSet<(int,int)> boxBlocks, int robx, int roby, int cols, int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (i == roby && j == robx)
+                Console.Write('@');
+            else if (wallBlocks.Contains((i, j)))
+                Console.Write('#');
+            else if (boxBlocks.Contains((i, j)))
+                Console.Write('[');
+            else if (boxBlocks.Contains((i, j - 1)))
+                Console.Write(']');
+            else
+                Console.Write('.');
+        }
+        Console.WriteLine("");
+    }
+}
 
 void displayGrid(char[][] grid, int rows, int cols, int robotx, int roboty)
 {
@@ -155,81 +214,88 @@ void Part2(string[] lines, List<string> directions, int rows, int cols)
     int robotx = 0;
     int roboty = 0;
     char[][] grid = new char[rows][];
-    for (int i = 0; i < rows; i++)
-    {
-        var line = lines[i];
-        grid[i] = new char[cols*2];
-        for (int j = 0; j < cols; j++)
-        {
-            if (line[j] == '@') // it's the robot.
-            {
-                robotx = j*2;
-                roboty = i;
-                grid[i][j*2] = '@';
-                grid[i][(j*2) + 1] = '.';
-            }
-            else if (line[j] == 'O') {
-                grid[i][j*2] = '[';
-                grid[i][(j*2) + 1] = ']';
-            }
-            else
-            {
-                grid[i][j*2] = line[j];
-                grid[i][(j*2) + 1] = line[j];
-            }
-        }
-    }
+
+    HashSet<(int, int)> wallBlocks = new();
+    HashSet<(int, int)> boxBlocks = new();
+    ExpandGrid(lines, rows, cols, ref robotx, ref roboty, wallBlocks, boxBlocks);
     cols = cols * 2;
-    //displayGrid(grid, rows, cols, robotx, roboty);
+    displayPosiGrid(wallBlocks, boxBlocks, robotx, roboty, cols, rows);
 
     foreach (var dirLine in directions)
     {
         for (int q = 0; q < dirLine.Length; q++)
         {
-
             Console.ReadKey();
             Console.Clear();
-            displayGrid(grid, rows, cols, robotx, roboty);
+            displayPosiGrid(wallBlocks, boxBlocks, robotx, roboty, cols, rows);
             Console.WriteLine($"Next move {dirLine[q]}");
             (int dx, int dy) = dirToChange(dirLine[q]);
+
             if (dirLine[q] == '<' || dirLine[q] == '>')
             {
-                if (doPush(dx, dy, robotx, roboty, grid))
-                {
-                    robotx += dx;
-                    roboty += dy;
-                    Console.WriteLine($"Direction {dirLine[q]} yields a move.");
-                    displayGrid(grid, rows, cols, robotx, roboty);
-                }
-                else
-                {
-                    Console.WriteLine($"Direction {dirLine[q]} no movement.");
-                    displayGrid(grid, rows, cols, robotx, roboty);
-                }
+                robotx = HorizontalFlow(robotx, roboty, wallBlocks, boxBlocks, dx);
             }
             else
             {
+                roboty = VerticalFlow(robotx, roboty, wallBlocks, boxBlocks, dy);
+            }
+            displayPosiGrid(wallBlocks, boxBlocks, robotx, roboty, cols, rows);
 
-                if (doPushUpDown(dy, robotx, robotx, roboty, grid))
-                {
-                    robotx += dx;
-                    roboty += dy;
-                    Console.WriteLine($"Direction {dirLine[q]} yields a move.");
-                    displayGrid(grid, rows, cols, robotx, roboty);
-                }
-                else
-                {
-                    Console.WriteLine($"Direction {dirLine[q]} no movement.");
-                    displayGrid(grid, rows, cols, robotx, roboty);
-                }
-            }
-            }
+        }
     }
 
     var total = lanternFishCalculation(grid, rows, cols, '[');
     Console.WriteLine($"Total is {total}");
+
+    int VerticalFlow(int robotx, int roboty, HashSet<(int, int)> wallBlocks, HashSet<(int, int)> boxBlocks, int dx)
+    {
+        return -1;
+    }
+
+    int HorizontalFlow(int robotx, int roboty, HashSet<(int, int)> wallBlocks, HashSet<(int, int)> boxBlocks, int dx)
+    {
+        int nextcol = robotx + dx;
+        (int _, int boxstart) = nextPosForBoxHorizontal(nextcol, roboty, dx);
+        HashSet<(int, int)> toMove = new();
+        while (true)
+        {
+            if (wallBlocks.Contains((roboty, nextcol)))
+                break; // we're done, move nothing.
+            if (boxBlocks.Contains((roboty, boxstart))) // it's a box
+            {
+                toMove.Add((roboty, boxstart));
+                (int _, boxstart) = nextPosToSearchAfterBoxHorizontal(boxstart + dx, roboty, dx);
+                nextcol = (dx == 1) ? boxstart : boxstart + 1; // (int _, boxstart) = nextPosForBoxHorizontal(nextcol, roboty, dx);
+            }
+            else // we've got a '.'
+            {
+                foreach (var mover in toMove)
+                {
+                    boxBlocks.Remove(mover);
+                    boxBlocks.Add((mover.Item1, mover.Item2 + dx));
+                }
+                robotx += dx;
+                break;
+            }
+        }
+
+        return robotx;
+    }
 }
-    void Part1(string[] lines, List<string> directions, int rows, int cols)
+
+(int,int) nextPosForBoxHorizontal(int x, int y, int dx)
+{
+    if (dx == 1) return (y, x);
+    else return (y, x - 1);
+}
+
+(int, int) nextPosToSearchAfterBoxHorizontal(int x, int y, int dx)
+{
+    if (dx == 1) return (y, x+1);
+    else return (y, x - 1);
+}
+
+void Part1(string[] lines, List<string> directions, int rows, int cols)
 {
     int robotx = 0;
     int roboty = 0;
@@ -280,3 +346,4 @@ void Part2(string[] lines, List<string> directions, int rows, int cols)
     var total = lanternFishCalculation(grid, rows, cols, 'O');
     Console.WriteLine($"Total is {total}");
 }
+
