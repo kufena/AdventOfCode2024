e760285ef361a4c8846af93732695878ff5607f9
Andrew Douglas <fairtomiddlingster@gmail.com>
2024-12-22 11:20:35 +0000

    A start on part 2 - calculating sale vals and diffs, but not much else. Run out of time at the moment.

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -5,7 +5,58 @@ Console.WriteLine("Hello, World!");
 int iters = 2000;
 var lines = File.ReadAllLines(args[0]);
 
-Part1(iters, lines);
+Part2(iters, lines);
+//Part1(iters, lines);
+
+//
+// Find a sequence of changes that gives a max bananas sale.
+//
+void Part2(int iters, string[] lines)
+{
+    int[][] diffs = new int[lines.Length][];
+    int[][] saleval = new int[lines.Length][];
+    long total = 0;
+    for(int r = 0; r < lines.Length; r++)
+    {
+        var line = lines[r];
+        diffs[r] = new int[iters];
+        saleval[r] = new int[iters];
+        long lastsecret = 0;
+        long secret = long.Parse(line);
+        for (int i = 0; i < iters; i++)
+        {
+            lastsecret = secret;
+            secret = SecretSteps(secret);
+            saleval[r][i] = (int)(secret % 10);
+            diffs[r][i] = i > 0 ? saleval[r][i] - saleval[r][i - 1] : 0;
+            //Console.WriteLine($"iter {i} -> {secret}");
+        }
+        total += secret;
+    }
+
+    Console.WriteLine(total);
+}
+
+// Straightforward apply the rules, get the numbers,
+// add them up.
+void Part1(int iters, string[] lines)
+{
+    long total = 0;
+    foreach (var line in lines)
+    {
+        long secret = long.Parse(line);
+        for (int i = 0; i < iters; i++)
+        {
+            secret = SecretSteps(secret);
+
+            //Console.WriteLine($"iter {i} -> {secret}");
+        }
+        total += secret;
+    }
+
+    Console.WriteLine(total);
+}
+
 
 long mix(long a, long b)
 {
@@ -29,20 +80,3 @@ long SecretSteps(long secret)
     return secret;
 }
 
-void Part1(int iters, string[] lines)
-{
-    long total = 0;
-    foreach (var line in lines)
-    {
-        long secret = long.Parse(line);
-        for (int i = 0; i < iters; i++)
-        {
-            secret = SecretSteps(secret);
-
-            //Console.WriteLine($"iter {i} -> {secret}");
-        }
-        total += secret;
-    }
-
-    Console.WriteLine(total);
-}
